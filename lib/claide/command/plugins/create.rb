6b702780c652c92b39bc01b5faae4d8ed14655ad
Orta Therox <orta.therox@gmail.com>
2016-07-24 00:02:27 -0400

    Initial stab at fixing integration issues

--- lib/claide/command/plugins/create.rb
+++ lib/claide/command/plugins/create.rb
@@ -9,17 +9,18 @@ module CLAide
       # template
       #
       class Create < Plugins
-        NAME_PREFIX = 'claide-'
 
         self.summary = 'Creates a new plugin'
-        self.description = <<-DESC
+        def self.description
+          <<-DESC
                 Creates a scaffold for the development of a new plugin
-                named `NAME` according to the CocoaPods best practices.
+                named `NAME` according to the #{ CLAide::Plugins.config.name } best practices.
 
                 If a `TEMPLATE_URL`, pointing to a git repo containing a
                 compatible template, is specified, it will be used
                 in place of the default one.
-        DESC
+          DESC
+        end
 
         self.arguments = [
           CLAide::Argument.new('NAME', true),
@@ -28,8 +29,10 @@ module CLAide
 
         def initialize(argv)
           @name = argv.shift_argument
-          unless @name.nil? || @name.empty? || @name.start_with?(NAME_PREFIX)
-            @name = NAME_PREFIX + @name.dup
+          prefix = CLAide::Plugins.config.plugin_prefix
+
+          unless @name.nil? || @name.empty? || @name.start_with?(prefix)
+            @name = prefix + @name.dup
           end
           @template_url = argv.shift_argument
           super
@@ -59,10 +62,6 @@ module CLAide
         extend CLAide::Executable
         executable :git
 
-        TEMPLATE_BASE_URL = 'https://github.com/CocoaPods/'
-        TEMPLATE_REPO = TEMPLATE_BASE_URL + 'cocoapods-plugin-template.git'
-        TEMPLATE_INFO_URL = TEMPLATE_BASE_URL + 'cocoapods-plugin-template'
-
         # Clones the template from the remote in the working directory using
         # the name of the plugin.
         #
@@ -72,14 +71,7 @@ module CLAide
           UI.section("-> Creating `#{@name}` plugin") do
             UI.notice "using template '#{template_repo_url}'"
             command = ['clone', template_repo_url, @name]
-            if method(:git!).arity == -1
-              git! command
-            else
-              # TODO: delete this conditional and use the other branch when
-              # 0.5.0 is released
-              require 'shellwords'
-              git! command.map(&:to_s).map(&:shellescape).join(' ')
-            end
+            git! command
           end
         end
 
@@ -99,12 +91,12 @@ module CLAide
           end
         end
 
-        # Checks if a template URL is given else returns the TEMPLATE_REPO URL
+        # Checks if a template URL is given else returns the Plugins.config URL
         #
         # @return String
         #
         def template_repo_url
-          @template_url || TEMPLATE_REPO
+          @template_url || CLAide::Plugins.config.plugin_template_url
         end
 
         # Shows a reminder to the plugin author to make a Pull Request
